0013fc5984aad554484b01404ba713d9bac78138
bhogamkarthik <karthik752b9@gmail.com>
2026-07-18 20:39:32 +0530

    pattern 19

--- Patterns.cpp/19.cpp
+++ Patterns.cpp/19.cpp
@@ -4,20 +4,58 @@ using namespace std;
 int main() 
 {
     int n = 5;
-    int start =1;
-
     
     for(int i=0; i<n; i++){
-        if (i%2 == 0){
-            start = 1;
-        }else{
-            start = 0;
+        for(int j=0; j<n-i; j++){
+            cout << "*";
         }
-        for(int j=0; j<i+1; j++){
-            cout << start;
-            start =1-start;
+
+        for(int j=0; j<i; j++){
+            cout << " ";
         }
+
+        for(int j=0; j<i; j++){
+            cout << " ";
+        }
+
+        for(int j=0; j<n-i; j++){
+            cout << "*";
+        }
+
         cout << endl;
+        
     }
+
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i+1; j++){
+            cout << "*";
+        }
+
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+
+        for(int j=0; j<i+1; j++){
+            cout << "*";
+        }
+
+        cout << endl;
+        
+    }
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
